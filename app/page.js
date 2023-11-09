466f5d735d09bce32c17a7b234049af27cda9d78
VISHAL <vishalstar895@gmail.com>
2023-11-09 22:50:34 +0530

    react update

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 "use client"
 import axios from 'axios'
-import React, { useState } from 'react'
+import Link from 'next/link'
+import React, { useState,useEffect  } from 'react'
 
 const page = () => {
+  const [users, setUsers] = useState([])
   const getUsers=async()=>{
-    const [users, setusers] = useState(second)
+   
     // const res = await axios.get("https://jsonplaceholder.typicode.com/users")
     // const data = res.data
     // console.log(data);
@@ -12,8 +14,16 @@ const page = () => {
 
     const{data} = await axios.get("https://jsonplaceholder.typicode.com/users")
     console.log(data);
-    setusers(data)
+    setUsers(data)
   } 
+
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+  
+
+
   return (
     <>
     <h1 className='text-center text-2xl font-bold' >This is Home</h1>
@@ -21,7 +31,11 @@ const page = () => {
     
     <button onClick={getUsers}  className='bg-green-600 my-4 text-white px-4 py-2 rounded font-bold '>GET DATA</button>
     <div className='p-8 bg-slate-200 mt-5 ml-3'>
-      {users.map((e))}
+     <ul>
+      {users.map((e)=>{
+        return <li>{e.name} -- <a href={`/${e.id}`}>Explore</a>  </li>
+      })}
+     </ul>
     </div>
 
     </>
